17002dd27eb0c58c5caee0740d7b8d1c9622debf
elinalo <elinalohmann@googlemail.com>
2023-12-29 00:32:39 +0100

    chore: replaced relative paths with absolute in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodosModule } from './todos/todos.module';
+
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
+import { TodosModule } from 'src/app/todos/todos.module';
 
 @NgModule({
   declarations: [
