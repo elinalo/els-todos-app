cafdcc26b4f351537663654eb5fb67965d924163
elinalo <elinalohmann@googlemail.com>
2023-12-29 01:01:21 +0100

    feat: add todos routing

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TodosComponent } from "src/app/todos/components/todos/todos.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [],
+  imports: [
+    TodosComponent, 
+    RouterModule.forChild(routes),
+  ],
 })
 export class TodosModule {
 
