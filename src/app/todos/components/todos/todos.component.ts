0a9a51e693ab82b92d61fb441d3330e9a1f49b11
elinalo <elinalohmann@googlemail.com>
2023-12-30 23:18:24 +0100

    feat: add header component with todo input

--- src/app/todos/components/todos/todos.component.ts
+++ src/app/todos/components/todos/todos.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    HeaderComponent
+  ],    
   templateUrl: './todos.component.html',
 })
 export class TodosComponent { }
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TodosComponent } from "src/app/todos/components/todos/todos.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
+    HeaderComponent,
     TodosComponent, 
     RouterModule.forChild(routes),
   ],
