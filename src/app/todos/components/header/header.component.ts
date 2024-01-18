82793565482cc38fde1493072ec6944406602eec
elinalo <elinalohmann@googlemail.com>
2024-01-18 21:20:01 +0100

    feat: replaced todosHeader with property; configured prettier formatter in settings.json

--- src/app/todos/components/header/header.component.ts
+++ src/app/todos/components/header/header.component.ts
@@ -2,26 +2,24 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-todos-header',
-    templateUrl: './header.component.html',
-    standalone: true,
-    imports: [
-        CommonModule
-    ],
+  selector: 'app-todos-header',
+  templateUrl: './header.component.html',
+  standalone: true,
+  imports: [CommonModule],
 })
-export class HeaderComponent { 
+export class HeaderComponent {
+  constructor() {}
 
-    constructor(){}
+  public todosHeader: string = 'todos';
+  public text: string = '';
 
-    public text: string = '';
+  changeText(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.text = target.value;
+    console.log('target.value', target.value); // TODO: rename for console logs or remove
+  }
 
-    changeText(event: Event): void {
-        const target = event.target as HTMLInputElement;
-        this.text = target.value;
-        console.log('target.value', target.value);  // TODO: rename for console logs or remove
-    }
-
-    addTodo(): void {
-        console.log('addTodo', this.text);
-    }
+  addTodo(): void {
+    console.log('addTodo', this.text);
+  }
 }
