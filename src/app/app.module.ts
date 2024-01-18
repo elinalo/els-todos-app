45f50a3785e25364329f88bc826037da9524bd6a
elinalo <elinalohmann@googlemail.com>
2024-01-19 00:36:27 +0100

    chore: removed unnecessary FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { TodosModule } from 'src/app/todos/todos.module';
@@ -10,7 +9,6 @@ import { TodosModule } from 'src/app/todos/todos.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     TodosModule,
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodosComponent } from 'src/app/todos/components/todos/todos.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FormsModule } from '@angular/forms';
 import { TodoDetails } from './components/todo-details/todo-details.component';
 
 const routes: Routes = [
