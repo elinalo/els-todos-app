1151255f57bdffaa9830d04a2fa7c020dc24a2ff
elinalo <elinalohmann@googlemail.com>
2024-01-19 16:19:00 +0100

    feat: added todo mockups

---
+++ src/app/mock data/mock-todos.ts
@@ -0,0 +1,13 @@
+import { Todo } from '../todos/models/todo.model';
+
+export const TODOS: Todo[] = [
+  { id: 12, title: 'Einkaufen' },
+  { id: 13, title: 'Bad putzen' },
+  { id: 14, title: 'Küche putzen' },
+  { id: 15, title: 'Staubsaugen' },
+  { id: 16, title: 'Termin Bank' },
+  { id: 17, title: 'Finanzen' },
+  { id: 18, title: 'Kino' },
+  { id: 19, title: 'Programmieren' },
+  { id: 20, title: 'Kochen' },
+];
